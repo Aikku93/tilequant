0480a60c1a9fff4dd3191e0f77155475664208de
Marco Köpcke <marco.koepcke@tudock.de>
2020-10-19 11:10:38 +0200

    Fixed indents to be tabs again

--- Qualetize.c
+++ Qualetize.c
@@ -52,45 +52,45 @@ struct BGRAf_t Qualetize(
 	int TileH = TilesData->TileH;
 	int32_t *TilePalIdx = TilesData->TilePalIdx;
 #if MEASURE_PSNR
-	      struct BGRAf_t  RMSE     = (struct BGRAf_t){0,0,0,0};
+		  struct BGRAf_t  RMSE	 = (struct BGRAf_t){0,0,0,0};
 #endif
-	const        uint8_t *PxSrcIdx = Image->ColPal ? Image->PxIdx  : NULL;
+	const		uint8_t *PxSrcIdx = Image->ColPal ? Image->PxIdx  : NULL;
 	const struct BGRA8_t *PxSrcBGR = Image->ColPal ? Image->ColPal : Image->PxBGR;
 	//! DITHER_FLOYDSTEINBERG only
-    struct BGRAf_t *PxDiffuse = TilesData->PxData;
-    //! DITHER_ORDERED only
-    struct BGRAf_t *PaletteSpread = TilesData->PxData;
+	struct BGRAf_t *PxDiffuse = TilesData->PxData;
+	//! DITHER_ORDERED only
+	struct BGRAf_t *PaletteSpread = TilesData->PxData;
 	if (DitherType != DITHER_NONE) {
-	    if (DitherType == DITHER_FLOYDSTEINBERG) {
-            for(y=0;y<ImgH;y++) for(x=0;x<ImgW;x++) PxDiffuse[y*ImgW+x] = (struct BGRAf_t){0,0,0,0};
-        } else {
-            for(i=0;i<MaxTilePals;i++) {
-                //! Find the mean values of this palette
-                int n;
-                struct BGRAf_t Mean = (struct BGRAf_t){0,0,0,0};
-                for(n=PalUnused;n<MaxPalSize;n++) Mean = BGRAf_Add(&Mean, &Palette[i*MaxPalSize+n]);
-                Mean = BGRAf_Divi(&Mean, MaxPalSize-PalUnused);
+		if (DitherType == DITHER_FLOYDSTEINBERG) {
+			for(y=0;y<ImgH;y++) for(x=0;x<ImgW;x++) PxDiffuse[y*ImgW+x] = (struct BGRAf_t){0,0,0,0};
+		} else {
+			for(i=0;i<MaxTilePals;i++) {
+				//! Find the mean values of this palette
+				int n;
+				struct BGRAf_t Mean = (struct BGRAf_t){0,0,0,0};
+				for(n=PalUnused;n<MaxPalSize;n++) Mean = BGRAf_Add(&Mean, &Palette[i*MaxPalSize+n]);
+				Mean = BGRAf_Divi(&Mean, MaxPalSize-PalUnused);
 
-                //! Compute slopes and store to the palette spread
-                //! NOTE: For some reason, it works better to use the square root as a weight.
-                //! This probably gives a value somewhere between the arithmetic mean and
-                //! the smooth-max, which should result in better quality.
-                struct BGRAf_t Spread = {0,0,0,0}, SpreadW = {0,0,0,0};
-                for(n=PalUnused;n<MaxPalSize;n++) {
-                    struct BGRAf_t d = BGRAf_Sub(&Palette[i*MaxPalSize+n], &Mean);
-                    d = BGRAf_Abs(&d);
-                    struct BGRAf_t w = BGRAf_Sqrt(&d);
-                    d = BGRAf_Mul(&d, &w);
-                    Spread  = BGRAf_Add(&Spread,  &d);
-                    SpreadW = BGRAf_Add(&SpreadW, &w);
-                }
-                PaletteSpread[i] = BGRAf_DivSafe(&Spread, &SpreadW, NULL);
+				//! Compute slopes and store to the palette spread
+				//! NOTE: For some reason, it works better to use the square root as a weight.
+				//! This probably gives a value somewhere between the arithmetic mean and
+				//! the smooth-max, which should result in better quality.
+				struct BGRAf_t Spread = {0,0,0,0}, SpreadW = {0,0,0,0};
+				for(n=PalUnused;n<MaxPalSize;n++) {
+					struct BGRAf_t d = BGRAf_Sub(&Palette[i*MaxPalSize+n], &Mean);
+					d = BGRAf_Abs(&d);
+					struct BGRAf_t w = BGRAf_Sqrt(&d);
+					d = BGRAf_Mul(&d, &w);
+					Spread  = BGRAf_Add(&Spread,  &d);
+					SpreadW = BGRAf_Add(&SpreadW, &w);
+				}
+				PaletteSpread[i] = BGRAf_DivSafe(&Spread, &SpreadW, NULL);
 #ifdef DITHER_NO_ALPHA
-                PaletteSpread[i].a = 0.0f;
+				PaletteSpread[i].a = 0.0f;
 #endif
-            }
-        }
-    }
+			}
+		}
+	}
 	for(y=0;y<ImgH;y++) for(x=0;x<ImgW;x++) {
 		int PalIdx = TilePalIdx[(y/TileH)*(ImgW/TileW) + (x/TileW)];
 
@@ -98,61 +98,61 @@ struct BGRAf_t Qualetize(
 		struct BGRAf_t Px, Px_Original; {
 			struct BGRA8_t p;
 			if(PxSrcIdx) p = PxSrcBGR[PxSrcIdx[y*ImgW + x]];
-			else         p = PxSrcBGR[         y*ImgW + x ];
+			else		 p = PxSrcBGR[		 y*ImgW + x ];
 			Px_Original = BGRAf_FromBGRA8(&p);
 			Px_Original = BGRAf_AsYCoCg(&Px_Original);
 			Px = Px_Original;
 		}
-        if (DitherType != DITHER_NONE) {
-            if (DitherType == DITHER_FLOYDSTEINBERG) {
-                //! Adjust for diffusion error
-                {
-                    struct BGRAf_t Dif = PxDiffuse[y * ImgW + x];
+		if (DitherType != DITHER_NONE) {
+			if (DitherType == DITHER_FLOYDSTEINBERG) {
+				//! Adjust for diffusion error
+				{
+					struct BGRAf_t Dif = PxDiffuse[y * ImgW + x];
 #ifdef DITHER_NO_ALPHA
-                    Dif.a = 0.0f;
+					Dif.a = 0.0f;
 #endif
-                    Dif = BGRAf_Muli(&Dif, DITHER_LEVEL);
-                    Px = BGRAf_Add(&Px, &Dif);
-                }
-            } else {
-                //! Adjust for dither matrix
-                {
-                    int Threshold = 0, xKey = x, yKey = x ^y;
-                    int Bit = DitherType - 1;
-                    do {
-                        Threshold = Threshold * 2 + (yKey & 1), yKey >>= 1; //! <- Hopefully turned into "SHR, ADC"
-                        Threshold = Threshold * 2 + (xKey & 1), xKey >>= 1;
-                    } while (--Bit >= 0);
-                    float fThres = Threshold * (1.0f / (1 << (2 * DitherType))) - 0.5f;
-                    struct BGRAf_t DitherVal = BGRAf_Muli(&PaletteSpread[PalIdx], fThres * DITHER_LEVEL);
-                    Px = BGRAf_Add(&Px, &DitherVal);
-                }
-            }
-        }
+					Dif = BGRAf_Muli(&Dif, DITHER_LEVEL);
+					Px = BGRAf_Add(&Px, &Dif);
+				}
+			} else {
+				//! Adjust for dither matrix
+				{
+					int Threshold = 0, xKey = x, yKey = x ^y;
+					int Bit = DitherType - 1;
+					do {
+						Threshold = Threshold * 2 + (yKey & 1), yKey >>= 1; //! <- Hopefully turned into "SHR, ADC"
+						Threshold = Threshold * 2 + (xKey & 1), xKey >>= 1;
+					} while (--Bit >= 0);
+					float fThres = Threshold * (1.0f / (1 << (2 * DitherType))) - 0.5f;
+					struct BGRAf_t DitherVal = BGRAf_Muli(&PaletteSpread[PalIdx], fThres * DITHER_LEVEL);
+					Px = BGRAf_Add(&Px, &DitherVal);
+				}
+			}
+		}
 		//! Find matching palette entry and store
 		int PalCol = FindPaletteEntry(&Px, Palette + PalIdx*MaxPalSize, MaxPalSize, PalUnused);
 		PxData[y*ImgW + x] = PalIdx*MaxPalSize + PalCol;
-        struct BGRAf_t Error = BGRAf_Sub(&Px_Original, &Palette[PxData[y*ImgW + x]]);
+		struct BGRAf_t Error = BGRAf_Sub(&Px_Original, &Palette[PxData[y*ImgW + x]]);
 		if (DitherType == DITHER_FLOYDSTEINBERG) {
-            //! Store error diffusion
-            if(y+1 < ImgH) {
-                if(x > 0) {
-                    struct BGRAf_t t = BGRAf_Muli(&Error, 3.0f/16);
-                    PxDiffuse[(y+1)*ImgW+(x-1)] = BGRAf_Add(&PxDiffuse[(y+1)*ImgW+(x-1)], &t);
-                }
-                if(1) {
-                    struct BGRAf_t t = BGRAf_Muli(&Error, 5.0f/16);
-                    PxDiffuse[(y+1)*ImgW+(x  )] = BGRAf_Add(&PxDiffuse[(y+1)*ImgW+(x  )], &t);
-                }
-                if(x+1 < ImgW) {
-                    struct BGRAf_t t = BGRAf_Muli(&Error, 1.0f/16);
-                    PxDiffuse[(y+1)*ImgW+(x+1)] = BGRAf_Add(&PxDiffuse[(y+1)*ImgW+(x+1)], &t);
-                }
-            }
-            if(x+1 < ImgW) {
-                struct BGRAf_t t = BGRAf_Muli(&Error, 7.0f/16);
-                PxDiffuse[(y  )*ImgW+(x+1)] = BGRAf_Add(&PxDiffuse[(y  )*ImgW+(x+1)], &t);
-            }
+			//! Store error diffusion
+			if(y+1 < ImgH) {
+				if(x > 0) {
+					struct BGRAf_t t = BGRAf_Muli(&Error, 3.0f/16);
+					PxDiffuse[(y+1)*ImgW+(x-1)] = BGRAf_Add(&PxDiffuse[(y+1)*ImgW+(x-1)], &t);
+				}
+				if(1) {
+					struct BGRAf_t t = BGRAf_Muli(&Error, 5.0f/16);
+					PxDiffuse[(y+1)*ImgW+(x  )] = BGRAf_Add(&PxDiffuse[(y+1)*ImgW+(x  )], &t);
+				}
+				if(x+1 < ImgW) {
+					struct BGRAf_t t = BGRAf_Muli(&Error, 1.0f/16);
+					PxDiffuse[(y+1)*ImgW+(x+1)] = BGRAf_Add(&PxDiffuse[(y+1)*ImgW+(x+1)], &t);
+				}
+			}
+			if(x+1 < ImgW) {
+				struct BGRAf_t t = BGRAf_Muli(&Error, 7.0f/16);
+				PxDiffuse[(y  )*ImgW+(x+1)] = BGRAf_Add(&PxDiffuse[(y  )*ImgW+(x+1)], &t);
+			}
 		}
 #if MEASURE_PSNR
 		//! Accumulate squared error
